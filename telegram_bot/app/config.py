5b1d1f4fc8957779970354982913266f92cd1055
2000muhammad <2000muhammad@users.noreply.github.com>
2026-07-08 18:17:40 +0500

    Use production domain for bot API

--- telegram_bot/app/config.py
+++ telegram_bot/app/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-SITE_BASE_URL = os.getenv("SITE_BASE_URL", "https://forum.mtutoshkent.uz:82").rstrip("/")
+SITE_BASE_URL = os.getenv("SITE_BASE_URL", "https://forum.mtutoshkent.uz").rstrip("/")
 SITE_API_KEY = os.getenv("TELEGRAM_API_KEY", "change-me-api-key")
 DEFAULT_LANG = os.getenv("DEFAULT_LANG", "ru")
