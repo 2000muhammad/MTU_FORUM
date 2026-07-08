83af6ec1164cc42e4274be3e3b9f3e15cb9922b2
Muhammad <100317702+2000muhammad@users.noreply.github.com>
2026-07-08 17:21:24 +0500

    Update SITE_BASE_URL to production URL

--- telegram_bot/app/config.py
+++ telegram_bot/app/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-SITE_BASE_URL = os.getenv("SITE_BASE_URL", "http://127.0.0.1:8000")
+SITE_BASE_URL = os.getenv("SITE_BASE_URL", "https://forum.mtutoshkent.uz/ru/:82")
 SITE_API_KEY = os.getenv("TELEGRAM_API_KEY", "change-me-api-key")
 DEFAULT_LANG = os.getenv("DEFAULT_LANG", "ru")
